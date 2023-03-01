b4435a706deb0312fd8123b08e3e7e366d7981b5
Nicoalbarrracin <126627476+Nicoalbarrracin@users.noreply.github.com>
2023-02-28 23:30:28 -0300

    Add files via upload

---
+++ Html/Java/html5.js
@@ -0,0 +1,16 @@
+
+alert ("Bienvenido a mi CV")
+
+document.getElementById('boton').onclick = function() {
+    document.body.style.backgroundColor= 'black';
+    document.body.style.color= 'white';
+
+
+}
+
+document.getElementById('botonDesactivarModoOscuro').onclick = function() {
+    document.body.style.backgroundColor= 'pink';
+    document.body.style.color= 'brown';
+
+
+}
